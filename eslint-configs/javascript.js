cfe4d549d5a5d866d1ea02221787261b7fd5222e
inferrinizzard <inferrinizzard@gmail.com>
2022-11-06 13:40:58 -0800

    add javascript eslint rules

---
+++ eslint-configs/javascript.js
@@ -0,0 +1,12 @@
+module.exports = {
+	'extends': ['eslint:recommended'],
+
+	'no-console': 'warn',
+	'require-atomic-updates': 'error',
+
+	'complexity': ['error', { max: 15 }],
+	'no-alert': 'error',
+	'no-else-return': 'error',
+	'no-param-reassign': 'error',
+	'yoda': ['error', 'never', { onlyEquality: true }],
+};
