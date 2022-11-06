cd2e0b3269662390208c1075ea38ebd11e83bac6
inferrinizzard <inferrinizzard@gmail.com>
2022-11-06 14:26:23 -0800

    add import rules

---
+++ eslint-configs/import.js
@@ -0,0 +1,14 @@
+module.exports = {
+	extends: ['plugin:import/errors', 'plugin:import/warnings'],
+	rules: {
+		'no-duplicate-imports': 2,
+		'import/export': 'error',
+		'import/first': 'error',
+		'import/order': ['error', { 'newlines-between': 'always' }],
+		'import/no-absolute-path': 'error',
+		'import/no-cycle': 'error',
+		'import/no-duplicates': 'error',
+		'import/no-extraneous-dependencies': 'error',
+		'import/no-useless-path-segments': 'error',
+	},
+};
