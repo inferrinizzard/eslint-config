26a0181dec57610d197c2fe1f289331e654a77b5
inferrinizzard <inferrinizzard@gmail.com>
2023-05-19 18:33:20 -0700

    add vitest config

---
+++ eslint-configs/vitest.js
@@ -0,0 +1,21 @@
+module.exports = {
+  plugins: ['vitest'],
+  env: {
+    'vitest-globals/env': true,
+  },
+  extends: ['plugin:vitest/recommended', 'plugin:vitest-globals/recommended'],
+  rules: {
+    'vitest/prefer-to-be': 'off',
+  },
+  overrides: [
+    {
+      files: ['**/*.test.{ts,tsx}'],
+      globals: {
+        vi: true,
+      },
+      rules: {
+        'import/first': 'off',
+      },
+    },
+  ],
+};
