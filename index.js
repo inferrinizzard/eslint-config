9be11f3fc83aaa4f42bc48de5cb3e914f5ac7ae6
inferrinizzard <inferrinizzard@gmail.com>
2023-08-07 21:44:27 -0700

    skip yaml reexport

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-module.exports = require('./.eslintrc.yaml');
+module.exports = {
+  env: { node: true, es6: true },
+	plugins: ['canonical'],
+  extends: [
+    "./config/import",
+    "./config/javascript",
+    "./config/react",
+    "./config/typescript",
+    "./config/vitest",
+
+    "plugin:json/recommended-with-comments",
+  ],
+  parserOptions: { tsconfigRootDir: __dirname },
+};
