9cff844a707e1aa72935ea6cef0865f553b81fc5
inferrinizzard <inferrinizzard@gmail.com>
2023-08-07 23:29:31 -0700

    update import rules

--- config/import.js
+++ config/import.js
@@ -1,14 +1,24 @@
 module.exports = {
-  extends: ['plugin:import/errors', 'plugin:import/warnings', 'plugin:import/typescript'],
+  extends: [
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+  ],
   rules: {
-    'no-duplicate-imports': 'error',
-    'import/export': 'error',
-    'import/first': 'error',
-    'import/order': ['error', { 'newlines-between': 'always' }],
-    'import/no-absolute-path': 'error',
-    'import/no-cycle': 'error',
-    'import/no-duplicates': 'error',
-    'import/no-extraneous-dependencies': 'error',
-    'import/no-useless-path-segments': 'error',
+    "sort-imports": [
+      "error",
+      {
+        ignoreDeclarationSort: true,
+      },
+    ],
+    "no-duplicate-imports": "error",
+    "import/first": "error",
+    "import/order": ["error", { "newlines-between": "always" }],
+    "import/newline-after-import": ["error", { count: 2 }],
+    "import/no-absolute-path": "error",
+    "import/no-cycle": "error",
+    "import/no-duplicates": "error",
+    "import/no-extraneous-dependencies": "error",
+    "import/no-useless-path-segments": "error",
   },
 };
